d2206629ccfeeb3a9b5d5973fc8c7a7f07f3a10a
timgrillmeier <tlgrillmeier@gmail.com>
2020-09-16 11:21:11 +1000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class=" js pointerevents" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width">
+    <title tabindex="1">Tim Grillmeier</title>
+    <script>
+      
+    </script>
+    <link rel="stylesheet" href="css/reset.css">
+    <style>
+
+    </style>
+  </head>
+  <body contenteditable="false">
+
+    
+
+  </body>
+</html>
